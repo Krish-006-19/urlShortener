869cbd923252bbb021bcbf54b201c235a2e81c09
Krish-006-19 <krish.ramena24@vit.edu>
2025-06-24 11:35:04 +0530

    backend

---
+++ routes/user.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const User = require('../models/user')
+const router = express.Router()
+
+router.post('/',async (req, res)=>{
+    const { userName, email, password } = req.body
+    try {
+    await User.create({ userName, email, password })
+    } catch (error) {
+        throw error
+    }
+})
+
+module.exports = router
